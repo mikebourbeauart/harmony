5a95cad111cc6686d8a192618d5f8c20e4de3073
Mike Bourbeau <borbs727@gmail.com>
2023-11-04 17:42:50 -0400

    add context vars

--- src/core/components/AppShell.tsx
+++ src/core/components/AppShell.tsx
@@ -3,11 +3,38 @@ import { CanvasContext } from "@/core/contexts/CanvasContext";
 import { useRef, useState } from "react";
 
 function AppShell({ children }: React.PropsWithChildren<{}>) {
-	const canvasRef = useRef<HTMLCanvasElement>(null);
-	const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+	const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
 	const [canvasContext, setCanvasContext] =
 		useState<CanvasRenderingContext2D | null>(null);
-	const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+	const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+	const [pixelRatio, setPixelRatio] = useState(0);
+	const [brushSize, setBrushSize] = useState(1);
+	const [brushPressure, setBrushPressure] = useState(1);
+	const [color, setColor] = useState([0, 0, 0]);
+	const [backgroundColor, setBackgroundColor] = useState([250, 250, 250]);
+	const [storage, setStorage] = useState(null);
+	const [brush, setBrush] = useState(null);
+	const [saveTimeOut, setSaveTimeOut] = useState(null);
+	const [wacom, setWacom] = useState(null);
+	const [i, setI] = useState(0);
+	const [mouseX, setMouseX] = useState(0);
+	const [mouseY, setMouseY] = useState(0);
+	const [container, setContainer] = useState(null);
+	const [foregroundColorSelector, setForegroundColorSelector] =
+		useState(null);
+	const [backgroundColorSelector, setBackgroundColorSelector] =
+		useState(null);
+	const [menu, setMenu] = useState(null);
+	const [about, setAbout] = useState(null);
+	const [flattenCanvas, setFlattenCanvas] = useState(null);
+	const [isFgColorSelectorVisible, setIsFgColorSelectorVisible] =
+		useState(false);
+	const [isBgColorSelectorVisible, setIsBgColorSelectorVisible] =
+		useState(false);
+	const [isAboutVisible, setIsAboutVisible] = useState(false);
+	const [isMenuMouseOver, setIsMenuMouseOver] = useState(false);
+	const [shiftKeyIsDown, setShiftKeyIsDown] = useState(false);
+	const [altKeyIsDown, setAltKeyIsDown] = useState(false);
 
 	return (
 		<CanvasContext.Provider
@@ -15,6 +42,30 @@ function AppShell({ children }: React.PropsWithChildren<{}>) {
 				canvas,
 				canvasContext,
 				screenSize,
+				pixelRatio,
+				brushSize,
+				brushPressure,
+				color,
+				backgroundColor,
+				storage,
+				brush,
+				saveTimeOut,
+				wacom,
+				i,
+				mouseX,
+				mouseY,
+				container,
+				foregroundColorSelector,
+				backgroundColorSelector,
+				menu,
+				about,
+				flattenCanvas,
+				isFgColorSelectorVisible,
+				isBgColorSelectorVisible,
+				isAboutVisible,
+				isMenuMouseOver,
+				shiftKeyIsDown,
+				altKeyIsDown,
 				setCanvas,
 				setCanvasContext,
 				setScreenSize,
--- src/core/contexts/CanvasContext.tsx
+++ src/core/contexts/CanvasContext.tsx
@@ -5,6 +5,30 @@ type CanvasContextType = {
 	canvas: HTMLCanvasElement | null;
 	canvasContext: CanvasRenderingContext2D | null;
 	screenSize: { width: number; height: number };
+	pixelRatio: number;
+	brushSize: number;
+	brushPressure: number;
+	color: number[];
+	backgroundColor: number[];
+	storage: Storage | null;
+	brush: any;
+	saveTimeOut: any;
+	wacom: any;
+	i: number;
+	mouseX: number;
+	mouseY: number;
+	container: any;
+	foregroundColorSelector: any;
+	backgroundColorSelector: any;
+	menu: any;
+	about: any;
+	flattenCanvas: any;
+	isFgColorSelectorVisible: boolean;
+	isBgColorSelectorVisible: boolean;
+	isAboutVisible: boolean;
+	isMenuMouseOver: boolean;
+	shiftKeyIsDown: boolean;
+	altKeyIsDown: boolean;
 	setCanvas: React.Dispatch<React.SetStateAction<HTMLCanvasElement | null>>;
 	setCanvasContext: React.Dispatch<
 		React.SetStateAction<CanvasRenderingContext2D | null>
@@ -18,6 +42,30 @@ const initialCanvasContext: CanvasContextType = {
 	canvas: null,
 	canvasContext: null,
 	screenSize: { width: 0, height: 0 },
+	pixelRatio: 0,
+	brushSize: 1,
+	brushPressure: 1,
+	color: [0, 0, 0],
+	backgroundColor: [250, 250, 250],
+	storage: null,
+	brush: null,
+	saveTimeOut: null,
+	wacom: null,
+	i: 0,
+	mouseX: 0,
+	mouseY: 0,
+	container: null,
+	foregroundColorSelector: null,
+	backgroundColorSelector: null,
+	menu: null,
+	about: null,
+	flattenCanvas: null,
+	isFgColorSelectorVisible: false,
+	isBgColorSelectorVisible: false,
+	isAboutVisible: false,
+	isMenuMouseOver: false,
+	shiftKeyIsDown: false,
+	altKeyIsDown: false,
 	setCanvas: () => {},
 	setCanvasContext: () => {},
 	setScreenSize: () => {},
